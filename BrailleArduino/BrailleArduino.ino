e7ca7bdf02a6b020517dac635766edd4eb61c0b2
kream <45693917+iseegr8tfuldeadppl@users.noreply.github.com>
2020-07-03 19:51:09 +0100

    new coordinate system

---
+++ BrailleArduino/BrailleArduino.ino
@@ -0,0 +1,312 @@
+#include <Stepper.h> 
+#define STEPS 200
+#include <ctype.h>
+
+// Define stepper motor connections and motor interface type. Motor interface type must be set to 1 when using a driver
+Stepper stepper(STEPS, 5, 6); // Pin 2 connected to DIRECTION & Pin 3 connected to STEP Pin of Driver
+Stepper stepper2(STEPS, 3, 4); // Pin 2 connected to DIRECTION & Pin 3 connected to STEP Pin of Driver
+Stepper stepper3(STEPS, 7, 8); // Pin 2 connected to DIRECTION & Pin 3 connected to STEP Pin of Driver
+
+//#define motorInterfaceType 1
+int maximum_width = 4550;
+int maximum_height = 2070;
+int maximum_medium = 690; // 1700 absolute maximum, 620 great value
+
+String coordinates_x = "", coordinates_y = "", coordinates_z = "";
+int x = 0, y = 0, z = maximum_medium;
+
+
+bool previous_method_1_has_ran = false;
+
+int poke_request = 0; // will be 0 or 1 according to current state and will be applied
+
+// x y axis step
+int stepo = 10;
+
+// z axis step
+int stepo2 = 10;
+
+String my_dude_coordinates_full_string = "";
+
+
+
+void setup() {
+  Serial.begin(9600);
+
+  stepper.setSpeed(500); // max 1000
+  stepper2.setSpeed(500); // max 1000
+  stepper3.setSpeed(500); // max 1000
+  Serial.println(
+    "Started!\n(note: X and Y axis step is " + 
+    String(stepo, DEC) + 
+    " and Z axis step is " + 
+    String(stepo2, DEC) + 
+    ")\na (go up)     z (forward)     e (go down)\nq (left)      s (backward)    d (right)\n\n" + 
+    "Or type coordinates as three numbers separated by 1 space each with the ranges 0-" + String(maximum_width, DEC) + " 0-" + String(maximum_height, DEC) + " 0/1 \n\n");
+}
+
+
+
+void loop() {
+
+  if (Serial.available() > 0) {
+    
+    // Step 1: read the incoming message:
+    char received = Serial.read();
+
+    // Step 2: check method 1 which is jogging the motor manually
+    bool method_1_has_ran = manually_stepping_motor(received);
+
+    // Step 3: check method 2 which is sending the motor to specific coordinates
+    if(!method_1_has_ran && !previous_method_1_has_ran){
+      automatically_stepping_motor(received);
+
+      // Step 4: save method_1_has_ran to cancel the \n that comes after it
+      previous_method_1_has_ran = method_1_has_ran;
+    }
+  }
+}
+
+
+
+void automatically_stepping_motor(char received){
+  
+  bool coords_are_queued = false;
+
+  if(isdigit(received) || received==' ' || received=='\n'){
+
+    if(received!='\n')
+      my_dude_coordinates_full_string += String(received);
+    else
+      coords_are_queued = check_coordinates_being_inputed(received);
+    
+  } else
+    my_dude_coordinates_full_string = ""; // \0 just means empty char
+
+  // Step 3.4: if both coordinates were inputted then go to them
+  if(coords_are_queued){
+
+    // Step 3.4.1: go to coordinates
+    // Step 3.4.1.1: x axis
+    int x_difference = coordinates_x.toInt()-x;
+    step_motors_this_much(x_difference, "x");
+
+    // Step 3.4.1.2: y axis
+    int y_difference = coordinates_y.toInt()-y;
+    step_motors_this_much(y_difference, "y");
+
+    // Step 3.4.1.3: z axis
+    run_poke_request();
+
+    // Step 3.4.3: aftermath cleanup & preparation to continue
+    // converting coordinates_x/y into an array is necessary or else the atoi() function won't convert correctly
+    x = coordinates_x.toInt();
+    y = coordinates_y.toInt();
+    
+    coordinates_x = "";
+    coordinates_y = "";
+    coordinates_z = "";
+    my_dude_coordinates_full_string = "";
+    
+    showcoords();
+  }
+}
+
+
+
+bool manually_stepping_motor(char received){
+
+  // Step 2: check if user wants directional stepping
+  boolean method_1_has_ran = true;
+  switch(received){
+    case 'e':
+      z_go_up();
+      break;
+    case 'a':
+      z_go_down();
+      break;
+    case 'q':
+      Serial.println("left");
+      if(x>=stepo){
+        x -= stepo;
+        stepper.step(-stepo);
+        stepper2.step(-stepo);
+      }
+      showcoords();
+      break;
+    case 'd':
+      Serial.println("right");
+      if(x<=maximum_width-stepo){
+        x += stepo;
+        stepper.step(stepo);
+        stepper2.step(stepo);
+      }
+      showcoords();
+      break;
+    case 's':
+      Serial.println("backward");
+      if(y>=stepo){
+        y -= stepo;
+        stepper.step(-stepo);
+        stepper2.step(stepo);
+      }
+      showcoords();
+      break;
+    case 'z':
+      Serial.println("forward");
+      if(y<=maximum_height-stepo){
+        y += stepo;
+        stepper.step(stepo);
+        stepper2.step(-stepo);
+      }
+      showcoords();
+      break;
+    default:
+      method_1_has_ran = false;
+      Serial.println("entered default mode.");
+      break;
+  }
+  
+  return method_1_has_ran;
+}
+
+
+
+bool check_coordinates_being_inputed(char received){
+  
+  // loop through the string to extract all other tokens
+  if(strstr(my_dude_coordinates_full_string.c_str(), " ") != NULL) {
+    char * token = strtok(my_dude_coordinates_full_string.c_str(), " ");
+    int counter = 0;
+    
+    String temp_x, temp_y, temp_poke_request;
+    while( token != NULL ) {
+      counter += 1;
+      String token_no_star = String(atoi(token), DEC);
+
+      char tokenNoStar[1];
+      strcpy(tokenNoStar, token);
+
+      if(!isdigit(tokenNoStar[0])){
+        my_dude_coordinates_full_string = "";
+        Serial.println("coordinates must be integers");
+        return false;
+      }
+
+      if(counter==1){
+          temp_x = token_no_star;
+          if(token_no_star.toInt() <0 && token_no_star.toInt()>maximum_width){
+            Serial.println("Given x coordinate is out of range of travel please stay between 0 and " + String(maximum_width));
+            return false;
+          }
+      } else if(counter==2){
+          temp_y = token_no_star;
+          if(token_no_star.toInt()<0 && token_no_star.toInt()>maximum_height){
+            Serial.println("Given y coordinate is out of range of travel please stay between 0 and " + String(maximum_height));
+            return false;
+          }
+      } else if(counter==3){
+          temp_poke_request = token_no_star;
+          if(token_no_star!="1" && token_no_star!="0"){
+            Serial.println("Given z state is wrong please choose either 1 to go down or 0 to go up");
+            return false;
+          }
+      } else {
+        Serial.println("You entered more than 3 coordinates");
+        return false;
+      }
+      token = strtok(NULL, " ");
+    }
+    
+    if(counter==3){
+      coordinates_x = temp_x;
+      coordinates_y = temp_y;
+      poke_request = temp_poke_request.toInt();
+      return true;
+    }
+    
+  }
+  return false;
+}
+
+
+
+void showcoords() {
+  Serial.println("x: " + String(x, DEC) + " y: " + String(y, DEC) + " z: " + String(z, DEC) + " with poke_request: " + String(poke_request, DEC));
+}
+
+
+
+void z_go_down(){
+  Serial.println("going down");
+  int temp_maximum_medium2 = -maximum_medium;
+  while(true){
+    if(temp_maximum_medium2<=-stepo2){
+      temp_maximum_medium2 += stepo2;
+      stepper3.step(-stepo2);
+    } else {
+      stepper3.step(temp_maximum_medium2);
+      break;
+    }
+  }
+}
+
+
+
+void z_go_up(){
+  Serial.println("going up");
+  int temp_maximum_medium = maximum_medium;
+  while(true){
+    if(temp_maximum_medium>=stepo2){
+      temp_maximum_medium -= stepo2;
+      stepper3.step(stepo2);
+    } else {
+      stepper3.step(temp_maximum_medium);
+      break;
+    }
+  }
+}
+
+
+
+void run_poke_request(){
+
+  // if state is 1 poke the paper
+  if(poke_request==1){
+    z = 0;
+    z_go_down();
+    delay(3000);
+    z_go_up();
+  }
+}
+
+
+
+void step_motors_this_much(int difference, String axis){
+
+  // Prep: if it's y axis change accordingly
+  int stepper_step = stepo;
+  int stepper2_step = stepo;
+  if(axis=="y")
+    stepper2_step = -stepo;
+
+  // Prep 2 : if it's going left/backwards flip variables
+  if(difference<0){
+    difference = - difference;
+    stepper_step = -stepper_step;
+    stepper2_step = -stepper2_step;
+  }
+
+  // Main: do it
+  while(true){
+    if(difference>=stepo){
+      difference -= stepo;
+      stepper.step(stepper_step);
+      stepper2.step(stepper2_step);
+    } else {
+      stepper.step(difference);
+      stepper2.step(difference);
+      break;
+    }
+  }
+}
--- steppercontroller/steppercontroller.ino
+++ steppercontroller/steppercontroller.ino
@@ -5,17 +5,36 @@
 Stepper stepper(STEPS, 5, 6); // Pin 2 connected to DIRECTION & Pin 3 connected to STEP Pin of Driver
 Stepper stepper2(STEPS, 3, 4); // Pin 2 connected to DIRECTION & Pin 3 connected to STEP Pin of Driver
 Stepper stepper3(STEPS, 7, 8); // Pin 2 connected to DIRECTION & Pin 3 connected to STEP Pin of Driver
+
 //#define motorInterfaceType 1
-int x = 0, y = 0, z = 0;
 int maximum_width = 4550;
 int maximum_height = 2070;
-int maximum_medium = 1700;
+int maximum_medium = 690; // 1700 absolute maximum, 620 great value
+
+int x = 0, y = 0, z = maximum_medium;
+
+int z_state = 0; // will be 0 or 1 according to current state and will be applied
+int old_z_state = 0; // needed for confirmations
+    
+String coordinates_x = "", coordinates_y = "", coordinates_z = "";
+bool coordinates_x_ready = false, coordinates_y_ready = false, coordinates_z_ready = false;
+
+// x y axis step
+int stepo = 10;
+
+// z axis step
+int stepo2 = 10;
+
 
 void setup() {
+
   Serial.begin(9600);
-  stepper.setSpeed(0); // max 1000
-  stepper2.setSpeed(0); // max 1000
-  stepper3.setSpeed(0); // max 1000
+
+  stepper.setSpeed(500); // max 1000
+  stepper2.setSpeed(500); // max 1000
+  stepper3.setSpeed(500); // max 1000
+  Serial.println("Started!\n(note: X and Y axis step is " + String(stepo, DEC) + " and Z axis step is " + String(stepo2, DEC) + ")\na (go up)     z (forward)     e (go down)\nq (left)      s (backward)    d (right)\n\nOr type coordinates and it will go there!\n(note: enter the coordinates in the following order)\n\n   x-axis:\n      a number between 0 and " + String(maximum_width, DEC) + "\n   y-axis:\n      a number between 0 and " + String(maximum_height, DEC) + "\n   z-axis: (note: range is " + String(maximum_medium, DEC) + ")\n      0. to go up.\n      1. to go down");
+
 }
 
 bool is_a_number(char received){
@@ -37,77 +56,69 @@ bool is_a_number(char received){
 }
 
 void showcoords() {
-  Serial.println("x " + String(x, DEC) + " y " + String(y, DEC));
+  Serial.println("x: " + String(x, DEC) + " y: " + String(y, DEC) + " z: " + String(z, DEC) + " with state: " + String(z_state, DEC) + ".\n\n\n");
 }
 
-String coordinates_x = "", coordinates_y = "";
-bool coordinates_x_ready = false, coordinates_y_ready = false;
+void run_z_state(int state){
+
+  // if state is 1 go down
+  if(state==1){
+      z = 0;
+      Serial.println("going down");
+      int temp_maximum_medium2 = -maximum_medium;
+      while(true){
+        if(temp_maximum_medium2<=-stepo2){
+          temp_maximum_medium2 += stepo2;
+          stepper3.step(-stepo2);
+        } else {
+          stepper3.step(temp_maximum_medium2);
+          break;
+        }
+      }
+  
+  // if state is 0 go up
+  } else {
+      z = maximum_medium;
+      Serial.println("going up");
+      int temp_maximum_medium = maximum_medium;
+      while(true){
+        if(temp_maximum_medium>=stepo2){
+          temp_maximum_medium -= stepo2;
+          stepper3.step(stepo2);
+        } else {
+          stepper3.step(temp_maximum_medium);
+          break;
+        }
+      }
+  }
+}
+
+
+bool previous_method_1_has_ran = false;
 void loop() {
 
-  //if (Serial.available() > 0) {
+  if (Serial.available() > 0) {
     
     // Step 1: read the incoming message:
-    //char received = Serial.read();
-    char received = "";
+    char received = Serial.read();
 
-    // Step 1.5: detect z axis
-    
-    int stepo2 = 10;
-    while(true){
-      delay(350);
-      if(received=='g')
-        received='e';
-      else
-        received='g';
-      if(received=='g'){
-          Serial.println("going down");
-          stepper3.setSpeed(1000); // max 1000
-          int temp_maximum_medium2 = -maximum_medium;
-          while(true){
-            if(temp_maximum_medium2<=-stepo2){
-              temp_maximum_medium2 += stepo2;
-              stepper3.step(-stepo2);
-            } else {
-              stepper3.step(temp_maximum_medium2);
-              stepper3.setSpeed(0); // max 1000
-              break;
-            }
-          }
-          Serial.println("lmfao");
-          z = 0;
-      } else if(received=='e'){
-          Serial.println("going up");
-          stepper3.setSpeed(1000); // max 1000
-          int temp_maximum_medium = maximum_medium;
-          while(true){
-            if(temp_maximum_medium>=stepo2){
-              temp_maximum_medium -= stepo2;
-              stepper3.step(stepo2);
-            } else {
-              stepper3.step(temp_maximum_medium);
-              stepper3.setSpeed(0); // max 1000
-              break;
-            }
-          }
-          z = maximum_medium;
-      }
-    }
-    
     // Step 2: check if user wants directional stepping
-    int stepo = 10;
+    boolean method_1_has_ran = true;
     switch(received){
+      case 'e':
+        old_z_state = z_state;
+        z_state = 0;
+        break;
+      case 'a':
+        old_z_state = z_state;
+        z_state = 1;
+        break;
       case 'q':
         Serial.println("left");
         if(x>=stepo){
           x -= stepo;
-          showcoords();
-          stepper.setSpeed(500); // max 1000
-          stepper2.setSpeed(500); // max 1000
           stepper.step(-stepo);
           stepper2.step(-stepo);
-          stepper.setSpeed(0); // max 1000
-          stepper2.setSpeed(0); // max 1000
-          return;
         }
         showcoords();
         break;
@@ -115,14 +126,8 @@ void loop() {
         Serial.println("right");
         if(x<=maximum_width-stepo){
           x += stepo;
-          showcoords();
-          stepper.setSpeed(500); // max 1000
-          stepper2.setSpeed(500); // max 1000
           stepper.step(stepo);
           stepper2.step(stepo);
-          stepper.setSpeed(0); // max 1000
-          stepper2.setSpeed(0); // max 1000
-          return;
         }
         showcoords();
         break;
@@ -130,14 +135,8 @@ void loop() {
         Serial.println("backward");
         if(y>=stepo){
           y -= stepo;
-          showcoords();
-          stepper.setSpeed(500); // max 1000
-          stepper2.setSpeed(500); // max 1000
           stepper.step(-stepo);
           stepper2.step(stepo);
-          stepper.setSpeed(0); // max 1000
-          stepper2.setSpeed(0); // max 1000
-          return;
         }
         showcoords();
         break;
@@ -145,167 +144,195 @@ void loop() {
         Serial.println("forward");
         if(y<=maximum_height-stepo){
           y += stepo;
-          showcoords();
-          stepper.setSpeed(500); // max 1000
-          stepper2.setSpeed(500); // max 1000
           stepper.step(stepo);
           stepper2.step(-stepo);
-          stepper.setSpeed(0); // max 1000
-          stepper2.setSpeed(0); // max 1000
-          return;
         }
         showcoords();
         break;
       default:
-        //Serial.println("Enter:\n  1. To go left.\n  2. To go right.\n  3. To go forward.\n  4. To go backward.");
+        method_1_has_ran = false;
+        Serial.println("entered default mode.");
         break;
     }
 
-    // Step 3: check if user is inputting coordinates
-    if(is_a_number(received) || received=='\n'){
+    // Step 2.2: run z axis if requested
+    if(old_z_state!=z_state && method_1_has_ran){
+      Serial.println("entered here");
+      Serial.println("old_z_state " + String(old_z_state, DEC) + " z_state " + String(z_state, DEC));
+      old_z_state = z_state;
+      run_z_state(z_state);
+    }
 
-      // Step 3.1: convert serial input into string
-      String message = String(received);
+    // Pre-Step 3: if method 1 didn't run then check for method 2
+    if(!method_1_has_ran && !previous_method_1_has_ran){
 
-      // Step 3.2: check for coordinate_x
-      if(!coordinates_x_ready){
+      // Pre-Step 3: reset previous_method_1_has_ran
+      previous_method_1_has_ran = true;
+      
+      // Step 3: check if user is inputting coordinates
+      if(is_a_number(received) || received=='\n'){
   
-        // Step 3.2.1: serial sends \n at end of any serial input
-        if(message=="\n"){
+        // Step 3.1: convert serial input into string
+        String message = String(received);
+  
+        // Step 3.2: check for coordinates_x
+        if(!coordinates_x_ready){
+    
+          // Step 3.2.1: serial sends \n at end of any serial input
+          if(message=="\n"){
+            
+            // Step 3.2.2: double safety to check if we're here with coordinates and not an empty coordinate
+            if(coordinates_x!=""){
+              
+              // Step 3.2.3: if coordinate is within inforced range accept it, else clear variable
+              if(coordinates_x.toInt()>=0 && coordinates_x.toInt()<=maximum_width){
+                coordinates_x_ready= true;
+                Serial.println("caught x coordinate " + coordinates_x);
+              } else {
+                coordinates_x = "";
+                Serial.println("Given x coordinate is out of range of travel please stay between 0 and " + String(maximum_width));
+              }
+            }
+          } 
+          // Step 3.2.4: if we get here then we're still reading the full number just keep appending
+          else
+            coordinates_x += message;
+  
+  
+        // Step 3.3: check for coordinates_y
+        } else if(!coordinates_y_ready){
+  
+          // Step 3.3.1:  serial sends \n at end of any serial input
+          if(message=="\n"){
+  
+            // Step 3.3.2: double safety to check if we're here with coordinates and not an empty coordinate
+            if(coordinates_y!=""){
+  
+              // Step 3.3.3: if coordinate is within inforced range accept it, else clear variable
+              if(coordinates_y.toInt()>=0 && coordinates_y.toInt()<=maximum_height){
+                coordinates_y_ready= true;
+                Serial.println("caught y coordinate " + coordinates_y);
+              } else {
+                coordinates_y = "";
+                Serial.println("Given y coordinate is out of range of travel please stay between 0 and " + String(maximum_height));
+              }
+              
+            }
+          } 
+          // Step 3.3.4: if we get here then we're still reading the full number just keep appending
+          else
+            coordinates_y += message;
           
-          // Step 3.2.2: double safety to check if we're here with coordinates and not an empty coordinate
-          if(coordinates_x!=""){
-            
-            // Step 3.2.3: if coordinate is within inforced range accept it, else clear variable
-            if(coordinates_x.toInt()>=0 && coordinates_x.toInt()<=maximum_width){
-              coordinates_x_ready= true;
-              Serial.println("caught x coordinate " + coordinates_x);
-            } else {
-              coordinates_x = "";
-              Serial.println("Given x coordinate is out of range of travel please stay between 0 and " + String(maximum_width));
+        // Step 3.4: check for coordinates_z
+        } else if(!coordinates_z_ready){
+          
+          // Step 3.3.1:  serial sends \n at end of any serial input
+          if(message=="\n"){
+  
+            // Step 3.3.2: double safety to check if we're here with coordinates and not an empty coordinate
+            if(coordinates_z!=""){
+  
+              // Step 3.3.3: if coordinate is within inforced range accept it, else clear variable
+              if(coordinates_z=="0" || coordinates_z=="1"){
+                coordinates_z_ready= true;
+                old_z_state = z_state;
+                z_state = coordinates_z.toInt();
+                Serial.println("caught z state ");
+              } else {
+                coordinates_z = "";
+                Serial.println("Given z state is wrong please choose either 1 to go down or 0 to go up");
+              }
+              
             }
-          }
-        } 
-        // Step 3.2.4: if we get here then we're still reading the full number just keep appending
-        else
-          coordinates_x += message;
-
-
-      // Step 3.3: check for coordinate_y
-      } else if(!coordinates_y_ready){
-
-        // Step 3.3.1:  serial sends \n at end of any serial input
-        if(message=="\n"){
-
-          // Step 3.3.2: double safety to check if we're here with coordinates and not an empty coordinate
-          if(coordinates_y!=""){
-
-            // Step 3.3.3: if coordinate is within inforced range accept it, else clear variable
-            if(coordinates_y.toInt()>=0 && coordinates_y.toInt()<=maximum_height){
-              coordinates_y_ready= true;
-              Serial.println("caught y coordinate " + coordinates_y);
-            } else {
-              coordinates_y = "";
-              Serial.println("Given y coordinate is out of range of travel please stay between 0 and " + String(maximum_height));
-            }
-            
-          }
-        } 
-        // Step 3.3.4: if we get here then we're still reading the full number just keep appending
-        else
-          coordinates_y += message;
-        
-      }
-
-      // Step 3.4: if both coordinates were inputted then go to them
-      if(coordinates_y_ready){
-
-        // Step 3.4.1: go to coordinates
-        int x_difference = coordinates_x.toInt()-x;
-        if(x_difference>0){
-          stepper.setSpeed(500); // max 1000
-          stepper2.setSpeed(500); // max 1000
-          while(true){
-            if(x_difference>=10){
-              x_difference -= 10;
-              stepper.step(10);
-              stepper2.step(10);
-            } else {
-              stepper.step(x_difference);
-              stepper2.step(x_difference);
-              stepper.setSpeed(0); // max 1000
-              stepper2.setSpeed(0); // max 1000
-              break;
-            }
-          }
-        } else if(x_difference<0){
-          stepper.setSpeed(500); // max 1000
-          stepper2.setSpeed(500); // max 1000
-          while(true){
-            if(x_difference<=-10){
-              x_difference += 10;
-              stepper.step(-10);
-              stepper2.step(-10);
-            } else {
-              stepper.step(x_difference);
-              stepper2.step(x_difference);
-              stepper.setSpeed(0); // max 1000
-              stepper2.setSpeed(0); // max 1000
-              break;
-            }
-          }
+          } 
+          // Step 3.3.4: if we get here then we're still reading the full number just keep appending
+          else
+            coordinates_z = message;
         }
-
-        
-        int y_difference = coordinates_y.toInt()-y;
-
-        if(y_difference>0){
-          stepper.setSpeed(500); // max 1000
-          stepper2.setSpeed(500); // max 1000
-          while(true){
-            if(y_difference>=10){
-              y_difference -= 10;
-              stepper.step(10);
-              stepper2.step(-10);
-            } else {
-              stepper.step(y_difference);
-              stepper2.step(-y_difference);
-              stepper.setSpeed(0); // max 1000
-              stepper2.setSpeed(0); // max 1000
-              break;
+  
+        // Step 3.4: if both coordinates were inputted then go to them
+        if(coordinates_z_ready){
+  
+          // Step 3.4.1: go to coordinates
+          int x_difference = coordinates_x.toInt()-x;
+          if(x_difference>0){
+            while(true){
+              if(x_difference>=10){
+                x_difference -= 10;
+                stepper.step(10);
+                stepper2.step(10);
+              } else {
+                stepper.step(x_difference);
+                stepper2.step(x_difference);
+                break;
+              }
+            }
+          } else if(x_difference<0){
+            while(true){
+              if(x_difference<=-10){
+                x_difference += 10;
+                stepper.step(-10);
+                stepper2.step(-10);
+              } else {
+                stepper.step(x_difference);
+                stepper2.step(x_difference);
+                break;
+              }
             }
           }
-        } else if(y_difference<0){
-          stepper.setSpeed(500); // max 1000
-          stepper2.setSpeed(500); // max 1000
-          while(true){
-            if(y_difference<=-10){
-              y_difference += 10;
-              stepper.step(-10);
-              stepper2.step(10);
-            } else {
-              stepper.step(y_difference);
-              stepper2.step(-y_difference);
-              stepper.setSpeed(0); // max 1000
-              stepper2.setSpeed(0); // max 1000
-              break;
+  
+          
+          int y_difference = coordinates_y.toInt()-y;
+  
+          if(y_difference>0){
+            while(true){
+              if(y_difference>=10){
+                y_difference -= 10;
+                stepper.step(10);
+                stepper2.step(-10);
+              } else {
+                stepper.step(y_difference);
+                stepper2.step(-y_difference);
+                break;
+              }
             }
+          } else if(y_difference<0){
+            while(true){
+              if(y_difference<=-10){
+                y_difference += 10;
+                stepper.step(-10);
+                stepper2.step(10);
+              } else {
+                stepper.step(y_difference);
+                stepper2.step(-y_difference);
+                break;
+              }
+            }
+          } else if(z_state!=old_z_state){
+            Serial.println("no i enteredhere");
+            old_z_state = z_state;
+            run_z_state(z_state);
           }
+  
+          // Step 3.4.2: apply coordinates to x & y and then display it
+          x = coordinates_x.toInt();
+          y = coordinates_y.toInt();
+          
+          showcoords();
+  
+          // Step 3.4.3: clear variables
+          coordinates_x = "";
+          coordinates_y = "";
+          coordinates_z = "";
+          coordinates_x_ready = false;
+          coordinates_y_ready = false;
+          coordinates_z_ready = false;
         }
-
-        // Step 3.4.2: apply coordinates to x & y and then display it
-        x = coordinates_x.toInt();
-        y = coordinates_y.toInt();
-        
-        showcoords();
-
-        // Step 3.4.3: clear variables
-        coordinates_x = "";
-        coordinates_y = "";
-        coordinates_x_ready = false;
-        coordinates_y_ready = false;
       }
     }
 
- // }
+  
+    // Step 4: save method_1_has_ran to cancel the \n that comes after it
+    previous_method_1_has_ran = method_1_has_ran;
+  }
 }
